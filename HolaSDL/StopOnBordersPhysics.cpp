db3f8f068290952ad98ab8dd7378a5c5199e9f95
BMZE <blamacaz@ucm.es>
2017-04-16 20:53:52 +0200

    StopOnBordersPhysics hecho

--- HolaSDL/StopOnBordersPhysics.cpp
+++ HolaSDL/StopOnBordersPhysics.cpp
@@ -9,8 +9,31 @@ StopOnBordersPhysics::~StopOnBordersPhysics() {
 }
 
 void StopOnBordersPhysics::init(GameObject* o) {
+	
 }
 
 
 void StopOnBordersPhysics::update(GameObject* o) {
+	Vector2D<int> position = o->getPosition() + o->getDirection();
+
+	if (left_ && position.getX() <= 0)
+	{
+		o->setPosition(position.getX() + 5, position.getY());
+		o->setDirection(0, 0);
+	}
+	else if (right_ && position.getX() >= o->getGame()->getWindowHeight)
+	{
+		o->setPosition(position.getX() - 5, position.getY());
+		o->setDirection(0, 0);
+	}
+	else if (top_ && position.getY <= 0)
+	{
+		o->setPosition(position.getX(), position.getY() + 5);
+		o->setDirection(0, 0);
+	}
+	else if (bottom_ && position.getY() >= o->getGame()->getWindowHeight)
+	{
+		o->setPosition(position.getX(), position.getY() - 5);
+		o->setDirection(0, 0);
+	}
 }
