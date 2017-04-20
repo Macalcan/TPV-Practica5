f2186f5a3a427d0e220ffec089ddb2347a119ec7
BMZE <blamacaz@ucm.es>
2017-04-20 13:34:43 +0200

    PaddleAIPhysics pendiente de revision

--- HolaSDL/PaddleAIPhysics.cpp
+++ HolaSDL/PaddleAIPhysics.cpp
@@ -17,6 +17,7 @@ void PaddleAIPhysics::update(GameObject* paddle) {
 	Vector2D<int> dirBall = ball_->getDirection();
 	Vector2D<int> posPaddle = paddle->getPosition();
 	Vector2D<int> y_pred = ball_->getPosition() + ball_->getDirection();
+
 	if (dirBall.getX() < 0)
 	{	
 		if (posPaddle.getY() + paddle->getHeight() / 2 == paddle->getGame()->getWindowHeight() / 2
@@ -33,20 +34,27 @@ void PaddleAIPhysics::update(GameObject* paddle) {
 			paddle->setDirectionY(5);
 		}
 		
-		
 	}
-	/*else
+	else
 	{
-		if (y_pred.getY() > paddle->getGame()->getWindowHeight() / 2 && posPaddle.getY() != paddle->getHeight() + paddle->getHeight() / 2)
+		if (posPaddle.getY() == 0)
+		{
+			paddle->setDirectionY(5);
+		}
+		else if (posPaddle.getY() == paddle->getGame()->getWindowHeight() - paddle->getHeight())
 		{
 			paddle->setDirectionY(5 * -1);
 		}
-		else if (y_pred.getY() < paddle->getGame()->getWindowHeight() / 2 && posPaddle.getY() != paddle->getHeight() / 2)
+		else if (y_pred.getY() > paddle->getGame()->getWindowHeight() / 2)
+		{
+			paddle->setDirectionY(5 * -1);
+		}
+		else if (y_pred.getY() < paddle->getGame()->getWindowHeight() / 2)
 		{
 			paddle->setDirectionY(5);
 		}
 
-	}*/
+	}
 	paddle->setPosition(paddle->getPosition() + paddle->getDirection()); 
 
 }
